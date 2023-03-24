0fded780f2133e54da0dc74d5284f1fa58cd905a
Sarah Baughman <sarahbaughman92@gmail.com>
2023-03-24 15:52:18 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
 });
+
+
+ 
+
+document.addEventListener("DOMContentLoaded", () => { 
+
+  const createTaskForm = document.getElementById ('create-task-form') 
+  createTaskForm.addEventListener( 'submit' , addNewTask) 
+}) 
+  
+function addNewTask (event) { 
+
+  event.preventDefault() 
+  const ulTasks = document.getElementById( 'tasks' ) 
+  const liTask = document.createElement ( 'li') 
+  ulTasks.appendChild( liTask) 
+  let taskForm = event.target 
+  liTask.textContent = taskForm['new-task-description'].value
+
+} 
